1e839a0ad29b0349e3abe73376a30f6696eee5b5
tanver68 <16101068@uap-bd.edu>
2020-12-14 17:53:59 +0600

    pass data

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     <Nayok name={'jasim'}></Nayok>
+     <Nayok name={'kasim'}></Nayok>
+     <Nayok name={'lasim'}></Nayok>
+     <Nayok name={'casim'}></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
+}
+
+function  Nayok(props) {
+  const nayokStyle={border:'1px solid blue',
+                    margin:'10px' ,
+                  backgroundColor:'cyan'                   }
+    return <div style={nayokStyle}>
+      <h1>ami nayok -{props.name}</h1>
+    </div>
+  
 }
 
 export default App;
